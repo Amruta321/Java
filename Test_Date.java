eba95c8f32023d50a9f8156e54b9edf9d0610f43
Amruta321 <84777694+Amruta321@users.noreply.github.com>
2021-05-25 17:17:12 +0530

    java programs

---
+++ Test_Date.java
@@ -0,0 +1,11 @@
+
+public class Test_Date {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       Date d1=new Date();
+       d1.acceptInfo();
+       d1.display();
+	}
+
+}
---
+++ Test_Employee.java
@@ -0,0 +1,13 @@
+
+public class Test_Employee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1=new Employee();
+		e1.setValues();
+		e1.calculate();
+		e1.display();
+
+	}
+
+}
